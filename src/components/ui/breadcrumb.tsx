8f917c0ec8a853086b4606ac6cd7bb99f617415a
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-21 14:20:56 -0300

    style(components): added all shadcn/ui components to project

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -102,7 +102,7 @@ const BreadcrumbEllipsis = ({
     <span className='sr-only'>More</span>
   </span>
 )
-BreadcrumbEllipsis.displayName = 'BreadcrumbElipssis'
+BreadcrumbEllipsis.displayName = 'BreadcrumbElipsis'
 
 export {
   Breadcrumb,
@@ -111,5 +111,5 @@ export {
   BreadcrumbLink,
   BreadcrumbPage,
   BreadcrumbSeparator,
-  BreadcrumbEllipsis,
+  BreadcrumbEllipsis
 }
