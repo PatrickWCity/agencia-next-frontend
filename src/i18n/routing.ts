c08268cc4e63fd2bf4c8668f8eee4d879198c2cb
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-22 15:06:37 -0300

    feat(i18n): added Localized pathnames to i18n routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -6,7 +6,24 @@ export const routing = defineRouting({
   locales: ['en', 'es'],
 
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+
+  // The `pathnames` object holds pairs of internal and
+  // external paths. Based on the locale, the external
+  // paths are rewritten to the shared, internal ones.
+  pathnames: {
+    // If all locales use the same pathname, a single
+    // external path can be used for all locales
+    '/': '/',
+    '/blog': '/blog',
+
+    // If locales use different paths, you can
+    // specify each external path per locale
+    '/about': {
+      en: '/about',
+      es: '/sobre'
+    }
+  }
 })
 
 // Lightweight wrappers around Next.js' navigation APIs
