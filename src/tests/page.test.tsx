a05873b8f33b42b053866ca552fd9c5f0f25c131
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-22 15:35:28 -0300

    test(vitest): updated basic Vitest Unit Test with default i18n routing

--- src/tests/page.test.tsx
+++ src/tests/page.test.tsx
@@ -1,8 +1,25 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import Home from '@/app/page'
+import Page from '@/app/[locale]/page'
+import { NextIntlClientProvider } from 'next-intl'
+import English from '@/messages/en.json'
+import Spanish from '@/messages/es.json'
 
 test('Home Page', () => {
-  render(<Home />)
+  render(
+    <NextIntlClientProvider messages={English} locale='en'>
+      <Page />
+    </NextIntlClientProvider>
+  )
   expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+})
+test('Página de Inicio', () => {
+  render(
+    <NextIntlClientProvider messages={Spanish} locale='es'>
+      <Page />
+    </NextIntlClientProvider>
+  )
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Inicio' })
+  ).toBeDefined()
 })
