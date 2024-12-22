efbaea0b1fba723e2cc00b439af4e3c6645b3c01
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-21 21:11:19 -0300

    test(vitest): added path alias to Vitest Configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname
+    }
   }
 })
